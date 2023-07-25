6a736afa2d9eae54f4fd58db454470c5f47c81a4
bilal96aslam <58890948+bilal96aslam@users.noreply.github.com>
2023-07-25 21:17:59 +0500

    lazy column, box, row views

--- app/src/main/java/com/ibotics/jetpackcomposeapp/MainActivity.kt
+++ app/src/main/java/com/ibotics/jetpackcomposeapp/MainActivity.kt
@@ -4,9 +4,12 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -16,91 +19,62 @@ import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.ibotics.jetpackcomposeapp.ui.theme.JetpackcomposeAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            //define theme in our app
             JetpackcomposeAppTheme {
-                ScreenUi()
+                PreviewMethod()
             }
         }
     }
 }
 
+@Preview(showBackground = true, widthDp = 300, heightDp = 500)
 @Composable
-fun ScreenUi() {
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = {
-                    Text("Compose Title")
-                },
-                navigationIcon = {
-                    IconButton(onClick = { }) {
-                        Icon(Icons.Filled.Menu, contentDescription = "menu")
-                    }
-                },
-                actions = {
-                    IconButton(onClick = { }) {
-                        Icon(
-                            Icons.Filled.Notifications,
-                            contentDescription = "notification"
-                        )
-                    }
-                    IconButton(onClick = { }) {
-                        Icon(Icons.Filled.Search, contentDescription = "search")
-                    }
-                }
-            )
-        }, floatingActionButton = {
-            FloatingActionButton(onClick = { }) {
-                IconButton(onClick = {}) {
-                    Icon(Icons.Filled.Add, contentDescription = "add")
-                }
-            }
-        }
-    ) {
-        //Greeting(name = "Android")
-        ShowSwitch()
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
+fun PreviewMethod() {
     Text(
-        text = "Hello $name!", color = MaterialTheme.colors.secondary
+        text = "Hello",
+        color = Color.White,
+        modifier = Modifier.clickable {  }
+            .background(Color.Red)
+            .size(200.dp)
+            .padding(36.dp)
+            .border(4.dp, color = Color.White)
+            .clip(CircleShape)
+            .background(Color.Green)
     )
 }
 
-@Preview(showBackground = true, name = "light mode")
-/*@Preview(
-    showBackground = true, name = "dark mode",
-    uiMode = Configuration.UI_MODE_NIGHT_YES
-)*/
 @Composable
-fun DefaultPreview() {
-    JetpackcomposeAppTheme {
-        ScreenUi()
+fun PreviewMethod2() {
+    Row(
+       verticalAlignment = Alignment.CenterVertically
+    ) {
+        //work same as frame layout
+        Box{
+            Image(painter = painterResource(id = R.drawable.ic_launcher_background), contentDescription = "",
+            Modifier.size(50.dp))
+            Image(painter = painterResource(id = R.drawable.ic_launcher_foreground), contentDescription = "",
+            Modifier.size(30.dp))
+        }
+        Column() {
+            Text(text = "John Doe", fontSize = 22.sp, color = Color.Black, fontWeight = FontWeight.Bold)
+            Text(text = "Software Developer", fontSize = 16.sp, color = Color.Gray,
+                fontWeight = FontWeight.Thin)
+        }
     }
-}
 
-//switch composable function
-@Composable
-fun ShowSwitch() {
-    val isChecked = remember {
-        mutableStateOf(true)
-    }
-
-    Switch(checked = isChecked.value, onCheckedChange = {
-        isChecked.value = it
-    }, modifier = Modifier.run {
-        size(20.dp)
-        padding(100.dp)
-    })
 }
